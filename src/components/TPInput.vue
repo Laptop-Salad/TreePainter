73b6949f43ace82b6870715e52fa29ef90b46fb6
Laptop-Salad <awallis1609@gmail.com>
2025-01-28 00:47:12 +0000

    first upload

---
+++ src/components/TPInput.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{
+  label: string,
+  id: string,
+  placeholder?: string,
+}>()
+
+const [model, modifiers] = defineModel()
+</script>
+
+<template>
+  <div>
+    <label
+        :id="id"
+        class="block pb-2 text-sm font-medium text-gray-900 dark:text-white"
+    >
+      {{ label }}
+    </label>
+
+    <input
+        type="text"
+        :id="id"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        :placeholder="placeholder"
+        v-model="model"
+        required
+    />
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,110 @@
+<script setup>
+import {onMounted, ref, watch} from "vue";
+import Node from "@/objects/Node.js";
+import TpInput from "../components/TPInput.vue";
+
+let canvas;
+let ctx;
+let currentX;
+let currentY;
+let nodeArray = ref(['R', 'A', 'B', 'C', 'D', 'E', 'F', null, null, null, null, null, null, 'G']);
+
+let userArray = ref('');
+
+onMounted(() => {
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  currentX = window.innerWidth / 2;
+  currentY = 100;
+
+  ctx.font = '16px Arial';
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+
+  drawTree();
+})
+
+function drawTree() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  const root = insert(0);
+  traverse(root, currentX, currentY, 200);
+}
+
+function drawNode(value, x, y) {
+  if (value !== null) {
+    ctx.beginPath();
+    ctx.arc(x, y, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    ctx.fillText(value, x, y);
+  }
+}
+
+function insert(i) {
+  let root = null;
+
+  if (i < nodeArray.value.length) {
+    root = new Node(nodeArray.value[i]);
+
+    root.right = insert(2 * i + 1);
+    root.left = insert(2 * i + 2);
+  }
+
+  return root;
+}
+
+function traverse(root, x, y, offset) {
+  if (root != null) {
+    drawNode(root.value, x, y);
+
+    traverse(root.right, x - offset, y + 120, offset / 2);
+    traverse(root.left, x + offset, y + 120, offset / 2);
+  }
+}
+
+function handleUserArrChange() {
+  try {
+    let array = userArray.value.split(",");
+
+    array.forEach((element, index) => {
+      array[index] = element.replace(/ /g,'');
+
+      if (array[index] === 'null') {
+        array[index] = null;
+      }
+    })
+
+    nodeArray.value = array;
+
+    drawTree();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+watch(userArray, async () => {
+  handleUserArrChange();
+});
+</script>
+
+<template>
+  <div class="flex justify-center items-center">
+    <canvas id="canvas" class="bg-amber-200"></canvas>
+
+    <div class="absolute self-end p-5 m-4 w-1/2">
+      <div class="bg-white w-full rounded-md p-5">
+        <TpInput
+            id="array"
+            label="Array"
+            placeholder="R, A, B, C, D, E, F, null, null, null, null, null, null, 'G'"
+            v-model="userArray"
+        />
+      </div>
+    </div>
+  </div>
+</template>
